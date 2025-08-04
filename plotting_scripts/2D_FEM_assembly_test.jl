881005adce1ded7ca38e44b00e579e1bd9ad758e
Michael Hardman <29800382+mrhardman@users.noreply.github.com>
2025-08-04 09:53:57 +0100

    Porting feature introduced in the previous repo https://github.com/mabarnes/moment_kinetics/commit/57c5186558225e91a5f8713735ce5a6d79a787c8.
    This feature allows the integrals of the collision operator to be computed without assembling the collision operator.
    
    Test this feature by
    ```
    $ julia --project=./plotting_scripts -O3
    julia> using FokkerPlanck; include("plotting_scripts/2D_FEM_assembly_test.jl"); run_assembly_test(test_numerical_conserving_terms=true, test_self_operator=true, ngrid = 5, nelement_list=[16], boundary_data_option=multipole_expansion)
    ```
    Note that integrals from this method only match those from the existing integration method when the quadrature of the existing method is exact for the pressure, i.e., for `ngrid >=5`.

--- plotting_scripts/2D_FEM_assembly_test.jl
+++ plotting_scripts/2D_FEM_assembly_test.jl
@@ -221,7 +221,8 @@ end
             # enforce the boundary conditions on CC before it is used for timestepping
             enforce_vpavperp_BCs!(fkpl_arrays.CC,vpa,vperp)
             # make ad-hoc conserving corrections
-            conserving_corrections!(fkpl_arrays.CC,Fs_M,vpa,vperp,dummy_vpavperp)            
+            conserving_corrections!(fkpl_arrays.CC,Fs_M,fkpl_arrays)
+            #conserving_corrections!(fkpl_arrays.CC,Fs_M,vpa,vperp)
         end
         # calculate Rosenbluth potentials again as a standalone to G and dGdvperp
         calculate_rosenbluth_potentials_via_elliptic_solve!(fkpl_arrays.GG,fkpl_arrays.HH,fkpl_arrays.dHdvpa,fkpl_arrays.dHdvperp,
